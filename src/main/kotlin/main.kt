e7b2c8e29a26f62f373d8e1e0655d62b1f763c90
UseYourFist <spr47@mail.ru>
2022-04-02 19:12:14 +0700

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val likes = 100
+    if (likes == 1 || likes == 21 || likes == 31 || likes == 41 || likes == 51 || likes == 61 || likes == 71 || likes == 81 || likes == 91 || likes == 101) {
+        println("Понравилось $likes человеку")
+    } else {
+        println("Понравилось $likes людям")
+    }
+}
