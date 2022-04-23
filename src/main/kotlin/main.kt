2c2a3412b1286ee54814b0a32b5c5086b371b01b
UseYourFist <spr47@mail.ru>
2022-04-23 18:16:50 +0700

    fixed hardcode problem

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,12 @@
+
+
 fun main() {
-    val likes = 100
-    if (likes == 1 || likes == 21 || likes == 31 || likes == 41 || likes == 51 || likes == 61 || likes == 71 || likes == 81 || likes == 91 || likes == 101) {
+    val likes = 201
+    val residue = (likes % 10).toString()
+    val strLikes = likes.toString()
+    if (likes > 10 && ("${strLikes[strLikes.length - 1]}${strLikes[strLikes.length - 2]}") == "11") {
+        println("Понравилось $likes людям")
+    } else if ("${residue[residue.length - 1]}" == "1") {
         println("Понравилось $likes человеку")
     } else {
         println("Понравилось $likes людям")
